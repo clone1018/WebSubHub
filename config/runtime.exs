9f636f5ed3294c2b206d3a6053e7459f2e72afb2
Luke Strickland <luke@axxim.net>
2022-01-09 16:12:43 -0500

    Adding WebSubHub.com to the prod environment

--- config/runtime.exs
+++ config/runtime.exs
@@ -33,6 +33,7 @@ if config_env() == :prod do
       """
 
   config :websubhub, WebSubHubWeb.Endpoint,
+    url: [host: "websubhub.com", port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
