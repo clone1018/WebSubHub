f8b89c5b91cb342a5754c615f6e607377aa761e0
Luke Strickland <luke@axxim.net>
2022-01-09 16:01:20 -0500

    Adding release for migrations

---
+++ lib/websubhub/release.ex
@@ -0,0 +1,31 @@
+defmodule WebSubHub.Release do
+  @moduledoc """
+  Handles the migrations / loading required for running the app inside a release.
+  """
+
+  @app :websubhub
+
+  alias Ecto.Migrator
+
+  def migrate do
+    load_app()
+
+    for repo <- repos() do
+      {:ok, _, _} = Migrator.with_repo(repo, &Migrator.run(&1, :up, all: true))
+    end
+  end
+
+  def rollback(repo, version) do
+    load_app()
+    {:ok, _, _} = Migrator.with_repo(repo, &Migrator.run(&1, :down, to: version))
+  end
+
+  defp repos do
+    Application.fetch_env!(@app, :ecto_repos)
+  end
+
+  defp load_app do
+    Application.load(@app)
+    Application.ensure_all_started(:ssl)
+  end
+end
